2f4d5e6ec0234d2dbf51e6704e3b2118c9e4a0f0
张良 <562974055@qq.com>
2019-02-20 17:13:47 +0800

    create redis cluster in ubuntu

---
+++ start_redis_instances.sh
@@ -0,0 +1,6 @@
+sudo redis-server ./7000/redis.conf &
+sudo redis-server ./7001/redis.conf &
+sudo redis-server ./7002/redis.conf &
+sudo redis-server ./7003/redis.conf &
+sudo redis-server ./7004/redis.conf &
+sudo redis-server ./7005/redis.conf &
